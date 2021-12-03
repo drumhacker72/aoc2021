e1cf8ff5c6cf2a9c2126bd848da4b492d9a839b1
J. Nelson <drumhacker72@gmail.com>
2021-12-03 12:59:03 -0600

    filter_by only needs first rate bit on each iteration

--- day3/day3.exs
+++ day3/day3.exs
@@ -3,20 +3,24 @@ defmodule Day3 do
         Enum.zip_with(rows, &Function.identity/1)
     end
 
-    def find_rate(ratings, mode) do
-        avg = length(ratings) / 2
-        counts = ratings |> transpose |> Enum.map(fn digits -> Enum.count(digits, fn x -> x == ?1 end) end)
+    def rate_bit(bits, mode) do
+        avg = length(bits) / 2
+        count = Enum.count(bits, fn x -> x == ?1 end)
         cmp = case mode do
             :gamma   -> &>=/2
             :epsilon -> &</2
         end
-        Enum.map(counts, fn count -> if cmp.(count, avg), do: ?1, else: ?0 end)
+        if cmp.(count, avg) do ?1 else ?0 end
+    end
+
+    def find_rate(ratings, mode) do
+        ratings |> transpose |> Enum.map(fn bits -> rate_bit(bits, mode) end)
     end
 
     def filter_by(ratings, mode) do
-        c = ratings |> find_rate(mode) |> hd
-        matches = Enum.flat_map(ratings, fn [d | ds ] -> if d == c, do: [ds], else: [] end)
-        [c | if length(matches) == 1 do hd(matches) else filter_by(matches, mode) end]
+        target = ratings |> Enum.map(&hd/1) |> rate_bit(mode)
+        matches = Enum.flat_map(ratings, fn [b | rest] -> if b == target, do: [rest], else: [] end)
+        [target | if length(matches) == 1 do hd(matches) else filter_by(matches, mode) end]
     end
 end
 
